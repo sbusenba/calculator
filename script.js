4835d139d2f3e39e020a3880dec7ac29304e8628
Steven Busenbarrick <sbusenba@gmail.com>
2021-11-07 08:11:52 -0800

    added border around display and updated operator functionality with if statement for negative numbers

--- script.js
+++ script.js
@@ -3,23 +3,49 @@ function numButtonPush(e){
     inputDisplay.textContent += this.textContent
 }
 function operatorPush(e){
+    if ((inputDisplay.textContent === '')&&(this.textContent==='-')){
+        inputDisplay.textContent = inputDisplay.textContent + '-'
+
+    } else if((inputDisplay.textContent != '')&&(outputDisplay.textContent ==='')){
     operatorDisplay.textContent=this.textContent
     outputDisplay.textContent = inputDisplay.textContent
     inputDisplay.textContent=''
+    }else if((outputDisplay.textConent != '')&&(inputDisplay.textContent=== '')){
+        operatorDisplay.textContent=this.textContent
+    }
     
 }
 function equalsPush(){
     console.log(operatorDisplay.textContent)
+    console.log(inputDisplay.textContent)
     switch (operatorDisplay.textContent){
-    case '':
-        outputDisplay.textContent=inputDisplay.textContent
-        inputDisplay.textContent=''
-        break
-    case '+':
-        outputDisplay.textContent = Number(inputDisplay.textContent) + Number(outputDisplay.textContent)
-        inputDisplay.textContent=''
-        operatorDisplay.textContent=''
-        break
+        case '':
+            outputDisplay.textContent=inputDisplay.textContent
+            inputDisplay.textContent=''
+            break
+        case '+':
+            outputDisplay.textContent = Number(inputDisplay.textContent) + Number(outputDisplay.textContent)
+            inputDisplay.textContent=''
+            operatorDisplay.textContent=''
+            break
+        case '-':
+
+            outputDisplay.textContent = Number(inputDisplay.textContent) - Number(outputDisplay.textContent)
+            inputDisplay.textContent=''
+            operatorDisplay.textContent=''
+            
+            break
+        case 'x':
+            outputDisplay.textContent = Number(inputDisplay.textContent) * Number(outputDisplay.textContent)
+            inputDisplay.textContent=''
+            operatorDisplay.textContent=''
+            break
+        case '/':
+            outputDisplay.textContent = Number(inputDisplay.textContent) / Number(outputDisplay.textContent)
+            inputDisplay.textContent=''
+            operatorDisplay.textContent=''
+            break
+        
     }
 
 
